e996439df3a4f86fb57820fa9b9fc37f85b0f1a0
Catie Poggioli <cmp5987@rit.edu>
2020-11-30 21:04:35 -0500

    Working version of date picker and fab icon

--- app/src/main/java/edu/rit/cmp5987/budgetbuddy/EditEntryActivity.kt
+++ app/src/main/java/edu/rit/cmp5987/budgetbuddy/EditEntryActivity.kt
@@ -1,5 +1,6 @@
 package edu.rit.cmp5987.budgetbuddy
 
+import android.app.DatePickerDialog
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,12 +16,15 @@ import android.widget.Toast
 import com.dev.materialspinner.MaterialSpinner
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_edit_entry.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class EditEntryActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class EditEntryActivity : AppCompatActivity(){
 
-    private var list_of_items = arrayOf("Select Entry Type","Needs Type Expense", "Wants Type Expense", "Source of Income")
-    private lateinit var spinner : MaterialSpinner
-    private var selectedSpinnerValue : String? = null
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
+    lateinit var datePicker: DatePickerHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,39 +40,49 @@ class EditEntryActivity : AppCompatActivity(), AdapterView.OnItemSelectedListene
             onBackPressed()
         }
 
-        spinner = spinner_entry_type
-        spinner.getSpinner().onItemSelectedListener = this
+        //Spinner Setup
+        val spinnerTypeAdapter = ArrayAdapter.createFromResource(this,
+            R.array.entry_type, android.R.layout.simple_spinner_item)
+        // Specify the layout to use when the list of choices appears
+        spinnerTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Apply the adapter to the spinner
+        spinner_entry_type.adapter = spinnerTypeAdapter
 
-        // Create an ArrayAdapter using a simple spinner layout and languages array
-        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, list_of_items)
-        // Set layout to use when the list of choices appear
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        // Set Adapter to Spinner
-        spinner!!.setAdapter(aa)
-    }
-    override fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
-        // use position to know the selected item
-        if(position > 0)
-        {
-            spinner.setErrorEnabled(false)
-            spinner.setLabel("Entry Type")
-            Toast.makeText(this, list_of_items[position], Toast.LENGTH_LONG).show()
-            selectedSpinnerValue = list_of_items[position]
-        }
-        else{
-            selectedSpinnerValue = null
+        val spinnerFrequencyAdapter = ArrayAdapter.createFromResource( this, R.array.frequency_types, android.R.layout.simple_spinner_item)
+        spinnerFrequencyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner_frequency.adapter = spinnerFrequencyAdapter
+
+
+        datePicker = DatePickerHelper(this)
+        et_date.setOnClickListener {
+            showDatePickerDialog()
         }
     }
+    private fun showDatePickerDialog() {
+        val cal = Calendar.getInstance()
+        val d = cal.get(Calendar.DAY_OF_MONTH)
+        val m = cal.get(Calendar.MONTH)
+        val y = cal.get(Calendar.YEAR)
 
-    override fun onNothingSelected(arg0: AdapterView<*>) {
-    }
+        val minDate = Calendar.getInstance()
+        minDate.set(Calendar.DAY_OF_MONTH, 1)
+        datePicker.setMinDate(minDate.timeInMillis)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            else -> super.onOptionsItemSelected(item)
-        }
+        val maxDate = Calendar.getInstance()
+        maxDate.add(Calendar.MONTH, 1)
+        maxDate.set(Calendar.DAY_OF_MONTH, 0)
+        datePicker.setMaxDate(maxDate.timeInMillis)
+
+        datePicker.showDialog(d, m, y, object : DatePickerHelper.Callback {
+
+            override fun onDateSelected(dayofMonth: Int, month: Int, year: Int) {
+                val dayStr = if (dayofMonth < 10) "0${dayofMonth}" else "${dayofMonth}"
+
+                val mon = month + 1
+                val monthStr = if (mon < 10) "0${mon}" else "${mon}"
+
+                et_date.setText("${dayStr}/${monthStr}/${year}")
+            }
+        })
     }
 }
